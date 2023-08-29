970f6c371ef70c9c7f827da8c017fe080b7807db
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-08-29 08:47:46 +0200

    fix:publishing.

--- src/endpoints/publish_to_topic.py
+++ src/endpoints/publish_to_topic.py
@@ -5,8 +5,17 @@ from models.publish_req import PublishReq
 router = APIRouter()
 
 
+def get_configuration() -> dict[str, str]:
+    return {
+        "bootstrap.servers": "localhost:9092",
+    }
+
+
 @router.post("/topic/{topic_name}/publish")
 async def publish_to_topic(topic_name: str, req: PublishReq):
-    manager = KafkaProducerManager(topic_name).init()
-    manager.publish(req.key, req.payload)
+    configuration = get_configuration()
+    print(f"topic: {topic_name}, key: {req.key}, payload: {req.payload}")
+    manager = KafkaProducerManager(topic_name).init(configuration)
+    manager.produce(req.key, req.payload)
+    manager.flush()
     return {"status": "accepted"}
