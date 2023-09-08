ac5ca4fcc577c12b05eee6805f40734102352b43
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-09-08 19:08:55 +0200

    chore:securet provider.

--- src/secret_provider.py
+++ src/secret_provider.py
@@ -4,7 +4,7 @@ from environs import Env
 class SecretProvider:
     def __init__(self):
         self._env = Env()
-        self._env.read_env()
+        self._env.read_env(".env")
 
     def get_secret(self, secret_name: str) -> str:
         return self._env(secret_name)
